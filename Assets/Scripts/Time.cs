329cbaad9d467602fab13942c2a211f51086f919
dazai <mydataisnotforfbi@proton.me>
2023-03-13 21:38:32 +0100

    actual time testing

---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Time : MonoBehaviour
+{
+    public string time = "";
+    public string spawnHour = "";
+    public GameObject testObject;
+
+
+    void Update()
+    {
+    
+        time = DateTime.Now.ToString();
+        string hour = time[11].ToString() + time[12].ToString();
+        
+        if(hour == spawnHour)
+        {
+
+            testObject.SetActive(true);
+        }else
+        {
+            
+            testObject.SetActive(false);
+        }
+    }
+}
