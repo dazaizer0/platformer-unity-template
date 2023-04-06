e3582cd43653163e83b4a3d1b6a42796d8cb764e
dazai <mydataisnotforfbi@proton.me>
2023-04-06 17:58:47 +0200

    textures

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    
+
+    [Header("parametry")]
+
     public Transform target;
     public float smoothTime = 0.3f;
 
     public Vector3 offset;
     private Vector3 velocity;
 
+    [Header("obszar")]
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    void Update()
+    {
+        
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax), 
+            Mathf.Clamp(transform.position.y, yMin, yMax), 
+            transform.position.z);
+
+    }
     private void LateUpdate()
     {
-
+        
         transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothTime);
     }
 }
